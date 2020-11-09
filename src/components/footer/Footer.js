c758ef068ef92be5dc1a23dba139695b77964bd3
sureshparimi1 <suresh.parimi@pfizer.com>
2020-11-09 20:51:13 +0530

    added talks info

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,12 +10,12 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by Saad Pasta")}
+          {emoji("Made with ❤️ by Suresh Parimi")}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
-          <a href="https://github.com/saadpasta/developerFolio">
-            developerFolio
+          <a href="https://www.linkedin.com/in/sparimi/">
+            Suresh Parimi's Tester Portfolio
           </a>
         </p>
       </div>
